2e1d8de50d7abd22fba020503e5831fe3bfad751
vikash.k <vikash.k@media.net>
2017-07-31 22:27:49 +0530

    app url changed in config

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://fierce-reef-98461.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
