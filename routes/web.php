f346dda0105904b419298a5d468ec36cbe038bf4
vikash.k <vikash.k@media.net>
2017-08-17 04:50:56 +0530

    login redirect issue fixed

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::group(['namespace' => 'Auth'], function(){
     Route::get('/admin/login','LoginController@showLoginForm')->name('login');
     Route::post('/admin/login','LoginController@login')->name('login');
 
-    Route::get('/admin/register','RegisterController@showRegistrationForm')->name('register');
-    Route::post('/admin/register','RegisterController@register')->name('register');
+//    Route::get('/admin/register','RegisterController@showRegistrationForm')->name('register');
+//    Route::post('/admin/register','RegisterController@register')->name('register');
 
     Route::get('/admin/password_reset','ResetPasswordController@showResetForm')->name('password.request');
     Route::post('/admin/password_reset','ResetPasswordController@reset')->name('password.request');
